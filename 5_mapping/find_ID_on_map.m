7863f539d458b42c41130fb1f787273b13f58423
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-03-30 11:58:33 -0700

    find_ID_on_map() now returns indices

--- 5_mapping/find_ID_on_map.m
+++ 5_mapping/find_ID_on_map.m
@@ -1,7 +1,10 @@
 % chenzhe, 2018-02-09
 % find ID from a map
+%
+% chenzhe, 2018-03-28. add [suby, subx] as output, which can be used as
+% [indr, indc] for other purposes.
 
-function ids = find_ID_on_map(X,Y,ID,f,a)
+function [ids, indr, indcx] = find_ID_on_map(X,Y,ID,f,a)
     set(f,'currentaxes',a);
     [x,y] = getpts;
     [nr,nc] = size(ID);
@@ -9,5 +12,7 @@ function ids = find_ID_on_map(X,Y,ID,f,a)
        [~,subx] = min(abs(X(1,:)-x(ii)));
        [~,suby] = min(abs(Y(:,1)-y(ii)));
        ids(ii) = ID(suby,subx);
+       indr(ii) =  suby;
+       indc(ii) =  subx;
     end
 end
