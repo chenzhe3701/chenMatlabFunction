e11542f0c3fa4d92cc05c1f606eb2937aab8298a
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-05-28 17:05:11 -0700

    add function to calculate_pos_on_IPF map. Add comments to the original code plot_on_IPF.

--- 1_crystallography/calculate_pos_on_IPF.m
+++ 1_crystallography/calculate_pos_on_IPF.m
@@ -1,24 +1,39 @@
-%  chenzhe 2016-4-5 revised
+function [x_out,y_out] = calculate_pos_on_IPF(eulers_d, str_material, varargin)
+% eulers_d: nx3 vector of euler angles
+% str_material = 'Ti' or 'Mg', maybe others later
+% 'sample_direction_of_interestI', default [1 0 0]
+% 'phi_sys', default [0 0 0]
+% 'phi_error', default [0 0 0]
+% 'stressTensor', default [1 0 0; 0 0 0; 0 0 0]
+% 'str_twin', default 'twin', could be 'pyii' maybe others
+% 'plotTF', default 'false'
 %
-%  This code plots an inverse pole figure, i.e., a direction of the sample projected in the crystal's coordinate system.
-%  Note: for different c_a ratio, the same point in IPF represents diffrent crystallographic direction.
-%  So, don't considers the position of the poles to be propotional...
-%  Zhe Chen, rewritten based on the structure built by Hongmei & Dr. Bieler
-%  2011-08-25
-%
-% chenzhe, 2017-07-25, make it a function
-%
-% sample_direction_of_interest: if it's [0 0 1], it's the normal direction of the sample
+% chenzhe, 2018-05-28, based on plot_on_IPF.
 
-function [] = plot_on_IPF(eulers_d, phi_sys, phi_error, sample_direction_of_interest, ss_of_interest, stressTensor, str_material, str_twin)
+p = inputParser;
 
-clc;
-figure;
-hold on;
-axis square;axis off;
-axis([0 1.2 0 1.2]);
+addParameter(p,'sample_direction_of_interest',[1 0 0]);
+addParameter(p,'phi_sys',[0 0 0]);
+addParameter(p,'phi_error',[0 0 0]);
+addParameter(p,'ss_of_interest',1);
+addParameter(p,'stressTensor',[1 0 0; 0 0 0; 0 0 0]);
+addParameter(p,'str_twin','twin');
+addParameter(p,'plotTF',false);
+
+parse(p,varargin{:});
+
+
+phi_sys = p.Results.phi_sys; 
+phi_error = p.Results.phi_error;
+sample_direction_of_interest = p.Results.sample_direction_of_interest;
+ss_of_interest = p.Results.ss_of_interest;
+stressTensor = p.Results.stressTensor;
 
 stressTensor = stressTensor/norm(stressTensor);
+
+str_twin = p.Results.str_twin;
+plotTF = p.Results.plotTF;
+
 % here if I write [1 0 0], it means tensile axis is the interested direction
 
 [ssa, c_a, nss, ntwin, ssGroup] = define_SS(str_material,str_twin);
@@ -34,7 +49,12 @@ ss = hex_to_cart_ss(ssa,c_a);
 theta_d = [0:1:30];  % just the paremeter 'theta'
 border_x = [0 cosd(theta_d) 0];
 border_y = [0 sind(theta_d) 0];
-plot(border_x, border_y, 'k-');
+if plotTF
+    figure; hold on;
+    axis square; % axis off;
+    axis([0 1.2 0 1.2]);
+    plot(border_x, border_y, 'k-');
+end
 % border_x2 = [0 cosd(theta)*sqrt(3)/3 0];  % this is for plotting 60_degree line
 % border_y2 = [0 sind(theta)*sqrt(3)/3 0];
 % plot(border_x2, border_y2, 'k-');
@@ -74,17 +94,20 @@ for i = 1:size(eulers_d,1)
     r = sqrt(X^2+Y^2);
     X = r*cosd(alpha);  % change the coord. X,Y into the plot region
     Y = r*sind(alpha);
-    
-    if max(SF(ss_of_interest)) > 0.4
-        plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[1 0 0],'MarkerSize',6);
-    elseif max(SF(ss_of_interest)) > 0.3
-        plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[1 1 0],'MarkerSize',6);
-    elseif max(SF(ss_of_interest)) > 0.2
-        plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[0 1 0],'MarkerSize',6);
-    elseif max(SF(ss_of_interest)) > 0.1
-        plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor', [0 0 1],'MarkerSize',6);
-    else
-        plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor', [0 0 0],'MarkerSize',6);
+    x_out(i) = X;
+    y_out(i) = Y;
+    if plotTF
+        if max(SF(ss_of_interest)) > 0.4
+            plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[1 0 0],'MarkerSize',6);
+        elseif max(SF(ss_of_interest)) > 0.3
+            plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[1 1 0],'MarkerSize',6);
+        elseif max(SF(ss_of_interest)) > 0.2
+            plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor',[0 1 0],'MarkerSize',6);
+        elseif max(SF(ss_of_interest)) > 0.1
+            plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor', [0 0 1],'MarkerSize',6);
+        else
+            plot(X,Y,'o','LineWidth',1,'MarkerEdgeColor',[1 1 1], 'MarkerFaceColor', [0 0 0],'MarkerSize',6);
+        end
     end
     
     %     % plot basal red, prism blue, pryII black, tension twin green.
@@ -104,9 +127,11 @@ for i = 1:size(eulers_d,1)
     %     %text(X,Y+0.03,num2str(i))
 end
 
-text(-0.12,0.12,'0 0 0 1','fontsize',18)
-text(1.02,0.02,'2 -1 -1 0','fontsize',18)
-text(0.87,0.52,'1 0 -1 0','fontsize',18)
-hold off;
+if plotTF
+    text(-0.12,0.12,'0 0 0 1','fontsize',18)
+    text(1.02,0.02,'2 -1 -1 0','fontsize',18)
+    text(0.87,0.52,'1 0 -1 0','fontsize',18)
+    hold off;
+end
 
 
