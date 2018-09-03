152c2ef45cb6b9d5f99bdc693425501fc7cd3599
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-09-03 12:39:01 -0700

    combine two functions into the new 'label_map_with_ID'.

--- 5_mapping/@deprecated/label_grain.m
+++ 5_mapping/@deprecated/label_grain.m
@@ -1,29 +1,24 @@
-% hf = label_map_with_ID(X,Y,ID,hf,target_ID)
+% chenzhe, 2018-03-30
+% 
+% make a label on map with the ID interested in, to help locate the grain.
+%
+% hf = label_map_with_ID(X,Y,ID,hf)
 % X, Y, ID are matrice of the same size: coordinates and ID_map.
 % hf is the handle_of_figure
-%
-% Combine two functions 'label_map_with_ID' and 'label_grain'
-% if no 'target_ID' provided, label all grains.
-% 
-% chenzhe, 2018-09-03
 
-function hf = label_map_with_ID(X,Y,ID,hf,target_ID)
+function hf = label_grain(target_id,X,Y,ID,hf)
 
 set(0,'currentfigure',hf);
 
 hold on;
 
-if ~exist('target_ID','var')
-    target_ID = unique(ID(:));
-    target_ID = target_ID(target_ID~=0);
-end
 ID_reduced = ID(1:10:end,1:10:end);
 X_reduced = X(1:10:end,1:10:end);
 Y_reduced = Y(1:10:end,1:10:end);
 z = gca;
 z = z.ZLim(2);
-for ii = 1:length(target_ID)
-    id = target_ID(ii);
+for ii = 1:length(target_id)
+    id = target_id(ii);
     ind = (ID_reduced==id);
     x = mean(X_reduced(ind));
     y = mean(Y_reduced(ind));
