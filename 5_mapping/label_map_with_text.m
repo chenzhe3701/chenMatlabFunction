bcece2237a5eccfb1956f690920f75012bf2e9a5
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2019-12-07 14:33:18 -0800

    add a function to label map

---
+++ 5_mapping/label_map_with_text.m
@@ -0,0 +1,59 @@
+% hf = label_map_with_ID(X,Y,ID,hf,target_ID)
+% X, Y, ID are matrice of the same size: coordinates and ID_map.
+% hf is the handle_of_figure
+%
+% Combine two functions 'label_map_with_ID' and 'label_grain'
+% if no 'target_ID' provided, label all grains.
+% 
+% chenzhe, 2018-09-03
+
+function hf = label_map_with_text(X,Y,ID,hf,varargin)
+
+p = inputParser;
+
+addRequired(p,'X');
+addRequired(p,'Y');
+addRequired(p,'ID');
+addRequired(p,'hf');
+addParameter(p,'target_ID',[]);
+addParameter(p,'color',[]);
+addOptional(p,'text',[]);
+
+parse(p,X,Y,ID,hf,varargin{:});
+
+X = p.Results.X;
+Y = p.Results.Y;
+ID = p.Results.ID;
+hf = p.Results.hf;
+target_ID = p.Results.target_ID;
+color = p.Results.color;
+text_string = p.Results.text;
+
+set(0,'currentfigure',hf);
+
+hold on;
+
+if isempty(target_ID)
+    target_ID = unique(ID(:));
+    target_ID = target_ID(target_ID~=0);
+end
+if isempty(color)
+	color = 'k';
+end
+
+ID_reduced = ID(1:10:end,1:10:end);
+X_reduced = X(1:10:end,1:10:end);
+Y_reduced = Y(1:10:end,1:10:end);
+z = gca;
+z = z.ZLim(2);
+for ii = 1:length(target_ID)
+    id = target_ID(ii);
+    ind = (ID_reduced==id);
+    x = mean(X_reduced(ind));
+    y = mean(Y_reduced(ind));
+    text(x,y,z,{[num2str(id) ':']; text_string},'color',color,'fontsize',18);
+end
+
+hold off;
+
+end
