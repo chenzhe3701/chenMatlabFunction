7c6f71cde7ef595c8d28f9b8f281ee0247a669dc
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2020-04-17 02:30:12 -0700

    ca(): make axis equal. return handle.

--- 3_customBuiltin/ca.m
+++ 3_customBuiltin/ca.m
@@ -1,10 +1,10 @@
 % chenzhe, 2017-11-21
 % adjust colorbar according to the value in M
-function clim = ca(M)
+function [clim,caxis_handle] = ca(M)
 
 clim = quantile(M(:),[0.005, 0.995]);
 caxis(gca, [clim(1), clim(2)]);
-colorbar;
-axis equal;
+caxis_handle = colorbar;
+% axis equal;
 
 end
