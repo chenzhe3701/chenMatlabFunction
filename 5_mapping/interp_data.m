39272c024a8c1ea7af3116f257101911ad5b7fd2
Zhe Chen <chenzhezju@gmail.com>
2021-01-01 11:57:35 -0500

    interp_data(): add comments

--- 5_mapping/interp_data.m
+++ 5_mapping/interp_data.m
@@ -1,13 +1,28 @@
-% interpolate data using 'fit' or 'interp2' method
 % Dt = interp_data(xf,yf,Df,xt,yt,tform_fwd,method, interpMethod)
+% interpolate data using 'fit' or 'interp2' method
+% 
+% output = Dt: data matrix interp to
+% 
+% inputs:
+% xf: x coordinate matrix interp from
+% yf: y coordinate matrix interp from
+% Df: data matrix interp from
+% xt: x coordinate matrix interp to
+% yt: y coordinate matrix interp to
+% tform_fwd: this can have two forms:
+%   (1) structure from 'maketform'
+%   (2) object from 'fitgeotrans'
+%   If empty [], no transform, just interp
+% method:  'interp' = use function 'interp2', 'fit' = use function scatteredInterplant 
+% interpMethod: interp method in function such as 'nearest' or 'linear'
+%
+% chenzhe, 2020-12-31 organized note
 %
 % chenzhe, 2017-05-31
 % chenzhe, 2018-05-25, update to solve possible problems with EBSD
 % wrapping unwrapping. Use with caution.
 
 function Dt = interp_data(xf,yf,Df,xt,yt,tform_fwd,method,interpMethod)
-% if empty, no transform, just interp
-
 % correct EBSD unwrapping related issue. Use with caution.
 for iR = size(xf,1):-1:1
     if ~isequal(xf(iR,:),xf(1,:),'rows')
@@ -24,6 +39,16 @@ end
 if isempty(tform_fwd)
     tform_fwd = maketform('projective',diag([1 1 1]));
 end
+% get tform type. It can be a result of old version 'maketform', or can be
+% a result of 'fitgeotrans'
+tform_type = class(tform_fwd);
+switch tform_type
+    case 'affine2d'
+        tform_fwd =  maketform('affine', tform_fwd.T);
+    case'projective2d'
+        tform_fwd = maketform('projective', tform_fwd.T);
+end
+
 switch method
     case {'interp','Interp'}
         [xt_bwd, yt_bwd] = tforminv(tform_fwd,xt,yt);
