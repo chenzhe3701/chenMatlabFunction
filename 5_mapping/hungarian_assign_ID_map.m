8c7a640d4da6848b2c861890221f3e6b7f759463
Zhe Chen <chenzhezju@gmail.com>
2021-01-25 22:33:04 -0500

    modified hungarian_assign_ID_map: added notes, remove the output for zero-overlap pairs.

--- 5_mapping/hungarian_assign_ID_map.m
+++ 5_mapping/hungarian_assign_ID_map.m
@@ -25,15 +25,38 @@
 % priority. However, there is zero match between '2', and '12'. On the
 % other hand, if we don't obey the one-to-one priority algorithm, there
 % might be more bugs in the realization of the code. 
-% So, if there are 0-overlaps, we rcord them separately, and assign the id
-% with '0' values.
+% So, after solving the problem with hungarian_assign algorithm, we check
+% the actual overlap. If there are 0-overlaps, for the ID_1 on
+% ID_temp_input, we find the ID_2 on ID_target that has the max overlap with
+% it. We assign an ID_3 to the area with ID_1, and record [ID_3, ID_2] in
+% id_link_additional.
 % (2) If  ID_input has nans, they will be numbered as 0 first.
 % (3) ID_target/template should not have NaN values, otherwise if nan
 % values are majorities, the matches IDs might be zeros (converted from
 % nans)
+% (4) If input ids are more than target, there will be more additional
+% assignment. If input ids are less than target, the more ids in target
+% will be omitted.
+% 
+% example 1: [new, additional, link] =
+% hungarian_assign_ID_map([1 1 2], [10 10 10])  
+% new = [10 10 11]
+% additional = [11 10]
+% link = [1, 10]
+% 
+% example 2: [new, additional, zero_overlap, link] = 
+% hungarian_assign_ID_map([10 10 10], [1 1 2])
+% new = [1 1 1]
+% additional = []
+% link = [10, 1]
+% 
+% example 3: [new, additional, link] = 
+% hungarian_assign_ID_map([1,1,2,3,3] , [10,10,10,12,13])
+% new = [10 10 14 13 13]
+% additional = [14, 10]
+% link = [1 10; 3 13]
 
-
-function [ID_new, id_link_additional, id_link_zero_overlap, id_link] = hungarian_assign_ID_map(ID_temp_input, ID_target, max_previous_ID)
+function [ID_new, id_link_additional, id_link] = hungarian_assign_ID_map(ID_temp_input, ID_target, max_previous_ID)
 
 % should ignore ID = 0. First if there are nans, convert to 0
 % That's the reason in the code later, there are 'if worker(ii)>0' and 'if job(ii)~=0' 
@@ -88,8 +111,11 @@ job_a = job_full(ind_additional_match);
 % job = munkres(max(overlap(:))-overlap);
 
 % (1) one-to-one match
-id_link_zero_overlap = [];  % record the links with zero overlap
+% id_link_zero_overlap = [];  % record the links with zero overlap
 id_link = [];   % map from inputID to targetID
+id_link_additional = []; % apart from one to one match, ID_new is assigned based on max overlap. this record [ind on new map, id on target map]
+id_additional = max_previous_ID + 1;
+
 ID_new = ID_temp_input;
 for ii = 1:length(worker)
     % if this one has a match
@@ -97,13 +123,24 @@ for ii = 1:length(worker)
         id_in_temp = uniqueID_temp_input(worker(ii));
         % if there is a match, reassign
         if job(ii)~=0
-            id_in_target = uniqueID_target(job(ii));
+            
             if overlap(worker(ii),job(ii))>0
+                id_in_target = uniqueID_target(job(ii));
                 ID_new(ID_temp_input==id_in_temp) = id_in_target;
                 id_link = [id_link; id_in_temp, id_in_target];
             else
-                ID_new(ID_temp_input==id_in_temp) = 0;
-                id_link_zero_overlap = [id_link_zero_overlap; id_in_temp, id_in_target];
+                % if zero overlap, for worker(ii), find the job with max overlap to it 
+                [~, job_ii] = max(overlap(worker(ii),:));
+                id_in_target = uniqueID_target(job_ii);
+                
+                ID_new(ID_temp_input==id_in_temp) = id_additional;
+                id_link_additional = [id_link_additional; id_additional, id_in_target];
+                % after using, increment id_additional for the next assignment
+                id_additional = id_additional + 1;
+            
+                % --> due to algorithm, there is a match bewteen grain IDs. Record the original match between IDs, but assign '0' to the area with zero overlap.
+%                 ID_new(ID_temp_input==id_in_temp) = 0;   
+%                 id_link_zero_overlap = [id_link_zero_overlap; id_in_temp, id_in_target];
             end
         end
     end
@@ -111,8 +148,6 @@ end
 
 
 % (2) after one-to-one match, assign additional matches
-id_additional = max_previous_ID + 1;
-id_link_additional = [];
 
 for ii = 1:length(worker_a)
     % if this one has a match
